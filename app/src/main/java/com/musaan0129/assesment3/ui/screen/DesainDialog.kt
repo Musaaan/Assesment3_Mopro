e1aba3afde982b29849789a5f815d3953de0cc4b
Musaaan <mi5011487@gmail.com>
2025-06-13 22:40:55 +0700

    Mengganti nama dari hewan dialog menjadi desain dialog, dan memindahkan fungsi crop gambar ke desain dialog

---
+++ app/src/main/java/com/musaan0129/assesment3/ui/screen/DesainDialog.kt
@@ -0,0 +1,221 @@
+package com.musaan0129.assesment3.ui.screen
+
+import android.content.ContentResolver
+import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageOptions
+import com.canhub.cropper.CropImageView
+import com.musaan0129.assesment3.R
+import com.musaan0129.assesment3.model.Desain
+import com.musaan0129.assesment3.network.DesainApi
+import com.musaan0129.assesment3.ui.theme.Mobpro1Theme
+
+@Composable
+fun DesainDialog(
+    desain: Desain? = null,
+    onDismissRequest: () -> Unit,
+    onConfirmation: (String, String, Double, Bitmap?) -> Unit
+) {
+    val context = LocalContext.current
+
+
+    var judul by remember { mutableStateOf("") }
+    var luas by remember { mutableStateOf("") }
+    var harga by remember { mutableStateOf("") }
+
+    var bitmap: Bitmap? by remember { mutableStateOf(null) }
+    val launcher = rememberLauncherForActivityResult(CropImageContract()) {
+        bitmap = getCroppedImage(context.contentResolver, it)
+    }
+
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = Modifier.padding(16.dp),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                if (desain != null && bitmap == null) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(
+                                DesainApi.getImageUrl(desain.id_desain)
+                            )
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = stringResource(R.string.gambar, desain.judul),
+                        contentScale = ContentScale.Crop,
+                        placeholder = painterResource(id = R.drawable.loading_img),
+                        error = painterResource(id = R.drawable.broken_img),
+                        modifier = Modifier.fillMaxWidth().aspectRatio(1f)
+                    )
+                } else {
+                    if (bitmap != null) {
+                        Image(
+                            bitmap = bitmap!!.asImageBitmap(),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxWidth().aspectRatio(1f)
+                        )
+                    }
+                }
+                OutlinedButton(
+                    onClick = {
+                        val options = CropImageContractOptions(
+                            null, CropImageOptions(
+                                imageSourceIncludeGallery = false,
+                                imageSourceIncludeCamera = true,
+                                fixAspectRatio = true
+                            )
+                        )
+                        launcher.launch(options)
+                    },
+                    modifier = Modifier.padding(top = 8.dp).fillMaxWidth()
+                ) {
+                    Text(text = stringResource(R.string.pilih_gambar))
+                }
+
+                OutlinedTextField(
+                    value = judul,
+                    onValueChange = { judul = it },
+                    label = { Text(text = stringResource(id = R.string.judul)) },
+                    maxLines = 1,
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.Words,
+                        imeAction = ImeAction.Next
+                    ),
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+
+                OutlinedTextField(
+                    value = luas,
+                    onValueChange = { luas = it },
+                    label = { Text(text = stringResource(id = R.string.luas)) },
+                    maxLines = 1,
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.Sentences,
+                        imeAction = ImeAction.Done
+                    ),
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+
+                OutlinedTextField(
+                    value = harga,
+                    onValueChange = {
+                        if (it.isEmpty() || it.matches(Regex("^\\d*\\.?\\d*\$"))) {
+                            harga = it
+                            if (it.toDoubleOrNull() != null && it.toDouble() <= 0.0) {
+                                harga = "1.0"
+                            }
+                        }
+                    },
+                    label = { Text(text = stringResource(id = R.string.harga)) },
+                    maxLines = 1,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        capitalization = KeyboardCapitalization.None,
+                        imeAction = ImeAction.Done
+                    ),
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    OutlinedButton(
+                        onClick = { onDismissRequest() },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(text = stringResource(R.string.batal))
+                    }
+
+                    OutlinedButton(
+                        onClick = { onConfirmation(judul, luas, harga.toDouble(), bitmap) },
+                        enabled = judul.isNotEmpty() && luas.isNotEmpty() && (harga.toDoubleOrNull() != null && harga.toDouble()  >= 1.0) && (desain != null || bitmap != null),
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(text = stringResource(R.string.simpan))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Suppress("deprecation")
+private fun getCroppedImage(
+    resolver: ContentResolver,
+    result: CropImageView.CropResult
+): Bitmap? {
+    if (!result.isSuccessful) {
+        Log.e("IMAGE", "Error: ${result.error}")
+        return null
+    }
+
+    val uri = result.uriContent ?: return null
+
+    return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+        MediaStore.Images.Media.getBitmap(resolver, uri)
+    } else {
+        val source = ImageDecoder.createSource(resolver, uri)
+        ImageDecoder.decodeBitmap(source)
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun AddDialogPreview() {
+    Mobpro1Theme {
+        DesainDialog(
+            desain = null,
+            onDismissRequest = {},
+            onConfirmation = { _, _, _, _ -> }
+        )
+    }
+}
+
